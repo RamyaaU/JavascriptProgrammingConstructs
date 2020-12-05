99e6246bb0cdf2f1135a6d5d5a11e04a6cfcf03f
Ramya.U <ramyayadav2016@gmail.com>
2020-12-05 14:42:18 +0530

    [Ramya] Add . Added method to add two numbers

---
+++ JavascriptClassProblems/add.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log("Addition is : "+c); 
+
+let s1 = "10";
+let s2 = "20";
+let s3 = s1 + s2;
+console.log("Addition is : "+c);
+
+let d =  new Date();
+let today = "Today is : " + (d.getMonth() + 1) + "/" + d.getDate() + "/" +d.getFullYear();
+let time = "Time: " + d.getHours() + ":" + (d.getMinutes());
+console.log("Todays date is : "+today);
+
